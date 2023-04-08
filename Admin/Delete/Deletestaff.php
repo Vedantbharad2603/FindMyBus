f8ac3829376a979fda8016d0cd2e759082af328f
vedant bharad <92723979+Vedantbharad2603@users.noreply.github.com>
2023-04-08 14:22:05 +0530

    08-04-2023

---
+++ Admin/Delete/Deletestaff.php
@@ -0,0 +1,17 @@
+<?php
+    require "../../includeFiles/connections.php";
+    try{
+        $sql1 = "DELETE FROM busschedule WHERE DriverId=? or ConductorId=?";
+        $statement= $pdo->prepare($sql1);
+        $statement->execute([$_REQUEST['id'],$_REQUEST['id']]);
+        
+        $sql2 = "DELETE FROM staff WHERE Id=?";
+        $statement= $pdo->prepare($sql2);
+        if($statement->execute([$_REQUEST['id']])) {
+            header("Location: ../adminHomepage.php");
+        }
+    }
+    catch(PDOException $e){
+        echo "Error : unable to execute the error".$e->getMessage();
+    }
+?>
